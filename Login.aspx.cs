e1f11c0ed2f788386b52e54614511bff1c3ce789
veinng <94824796+veinng@users.noreply.github.com>
2021-11-23 10:32:28 +0800

    Update Login.aspx.cs

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -1,3 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using YTL.Common;
+using System.Data;
+using System.Data.SqlClient;
+using APSCv4.Class;
+using YTL.Security.Login;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.IO;
+
+namespace APSCv4
+{
+    public partial class LoginV3 : System.Web.UI.Page
+    {
+
 private void MsgBox(string strMessage)
         {
             string strScript = "<SCRIPT LANGUAGE=\"JavaScript\">";
@@ -5,3 +26,6 @@ private void MsgBox(string strMessage)
             strScript += "</SCRIPT>";
             ClientScript.RegisterStartupScript(typeof(string), "messageBox", strScript);
         }
+        
+     }
+}
